744331608e1f60a9a0cb80d47b2dc1b8e0270a2f
lowleveldesign <lowleveldesign@localhost>
2014-04-18 09:37:35 +0200

    added EtwContextScope and new nuget package version

---
+++ nlog/LowLevelDesign.NLog.Ext/EtwContextScope.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace LowLevelDesign.NLog.Ext
+{
+    public sealed class EtwContextScope : IDisposable
+    {
+        private bool disposed = false;
+        private readonly Guid prevTraceActivityId;
+        private readonly Guid prevEtwActivityId;
+
+        public EtwContextScope()
+            : this(Guid.NewGuid())
+        {
+        }
+
+        public EtwContextScope(Guid activityId)
+        {
+            prevTraceActivityId = Trace.CorrelationManager.ActivityId;
+            Trace.CorrelationManager.ActivityId = activityId;
+            EventSource.SetCurrentThreadActivityId(activityId, out prevEtwActivityId);
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                disposed = true;
+                EventSource.SetCurrentThreadActivityId(prevEtwActivityId);
+                Trace.CorrelationManager.ActivityId = prevTraceActivityId;
+            }
+        }
+    }
+}
