9bddd3cd2075d0081d986c03b07b9855162e2fc2
Sebastian Solnica <ssolnica@gmail.com>
2020-10-13 17:42:55 +0200

    CPU sampling profiler code

---
+++ cpu-sampling-profiler/csharp/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.IO;
+using Microsoft.Diagnostics.Tracing.Session;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using System.Threading;
+using Microsoft.Diagnostics.Tracing.Etlx;
+using System.Linq;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+using Microsoft.Diagnostics.Symbols;
+using System.Collections.Generic;
+
+using NtKeywords = Microsoft.Diagnostics.Tracing.Parsers.KernelTraceEventParser.Keywords;
+using ClrKeywords = Microsoft.Diagnostics.Tracing.Parsers.ClrTraceEventParser.Keywords;
+
+namespace sampler
+{
+    class CallStackNode : Dictionary<string, CallStackNode>
+    {
+        public CallStackNode(int callsCount) : base(StringComparer.OrdinalIgnoreCase)
+        {
+            CallsCount = callsCount;
+        }
+
+        public int CallsCount { get; set; }
+    }
+
+    class Program
+    {
+        private const string sessionName = "mytrace";
+        private static readonly string etlFilePath = $"{sessionName}.etl";
+        private static TextWriter log = Environment.GetEnvironmentVariable("ENABLE_TRACEEVENT_LOG") == null ? TextWriter.Null : Console.Out;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 1 && args[0] == "trace")
+            {
+                if (TraceEventSession.IsElevated() == true)
+                {
+                    StartTrace();
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: You must be admin to trace the system.");
+                }
+            }
+            else if (args.Length >= 2 && args[0] == "analyze")
+            {
+                Analyze(args[1], args.Length > 2 ? Int32.Parse(args[2]) : 0);
+            }
+            else
+            {
+                Console.WriteLine("Usage: sampler trace | analyze <pid|process-name> [max-depth]");
+            }
+        }
+
+        private static void StartTrace()
+        {
+            void CollectRundownEvents()
+            {
+                Console.WriteLine("Forcing rundown of JIT methods.");
+
+                var rundownFilePath = Path.ChangeExtension(etlFilePath, "clrRundown.etl");
+                using var rundownSession = new TraceEventSession(sessionName + "-rundown", rundownFilePath);
+                rundownSession.EnableProvider(ClrRundownTraceEventParser.ProviderGuid, TraceEventLevel.Verbose,
+                    (ulong)ClrRundownTraceEventParser.Keywords.Default);
+
+                long length = -1;
+                while (true)
+                {
+                    var newLength = new FileInfo(rundownFilePath).Length;
+                    if (newLength == length)
+                    {
+                        return;
+                    }
+                    Thread.Sleep(2000);
+                    length = newLength;
+                }
+            }
+
+            using (var session = new TraceEventSession(sessionName, etlFilePath))
+            {
+                // kernel profiling events
+                var traceFlags = NtKeywords.Profile | NtKeywords.ImageLoad;
+                var stackFlags = NtKeywords.Profile;
+                session.EnableKernelProvider(traceFlags, stackFlags);
+
+                // JIT events required for stack resolution
+                session.EnableProvider(ClrTraceEventParser.ProviderGuid, TraceEventLevel.Verbose, (ulong)ClrKeywords.JITSymbols);
+
+                Console.Write("Press enter to stop the tracing session");
+                Console.ReadLine();
+
+                session.Stop();
+
+                CollectRundownEvents();
+                Console.WriteLine("Done with rundown");
+            }
+
+            Console.WriteLine("Collecting data required for stack resolution...");
+            var writer = new ZippedETLWriter(etlFilePath, log);
+            if (!writer.WriteArchive(System.IO.Compression.CompressionLevel.NoCompression))
+            {
+                Console.WriteLine("Error occured while merging");
+            }
+            else
+            {
+                Console.WriteLine("Trace session completed");
+            }
+        }
+        private static bool TryFindProcess(TraceLog traceLog, string processNameOrPid, [NotNullWhen(true)] out TraceProcess? process)
+        {
+            var filter = Int32.TryParse(processNameOrPid, out var pid) ?
+                (Func<TraceProcess, bool>)((TraceProcess p) => p.ProcessID == pid) :
+                    ((TraceProcess p) => String.Equals(p.Name, processNameOrPid, StringComparison.OrdinalIgnoreCase));
+
+            var processes = traceLog.Processes.Where(filter).ToArray();
+            if (processes.Length == 0)
+            {
+                process = null;
+                return false;
+            }
+            if (processes.Length == 1)
+            {
+                process = processes[0];
+                return true;
+            }
+            for (int i = 0; i < processes.Length; i++)
+            {
+                var p = processes[i];
+                Console.WriteLine($"[{i}] {p.Name} ({p.ProcessID}): '{p.CommandLine}'");
+            }
+            Console.Write("Which process to analyze []: ");
+            if (Int32.TryParse(Console.ReadLine(), out var ind) && 0 < ind && ind < processes.Length)
+            {
+                process = processes[ind];
+                return true;
+            }
+            process = null;
+            return false;
+        }
+
+        private static void LoadSymbols(TraceLog traceLog, TraceProcess proc)
+        {
+            using var symbolReader = new SymbolReader(log);
+            foreach (var m in proc.LoadedModules.Where(m => m.ModuleFile != null))
+            {
+                traceLog.CodeAddresses.LookupSymbolsForModule(symbolReader, m.ModuleFile);
+            }
+        }
+        private static CallStackNode AddOrUpdateChildNode(CallStackNode node, TraceCallStack callStack)
+        {
+            var decodeAddress = $"{callStack.CodeAddress.ModuleName}!{callStack.CodeAddress.FullMethodName}";
+            if (node.TryGetValue(decodeAddress, out var childNode))
+            {
+                childNode.CallsCount += 1;
+            }
+            else
+            {
+                childNode = new CallStackNode(1);
+                node.Add(decodeAddress, childNode);
+            }
+            return childNode;
+        }
+
+        private static CallStackNode ProcessStackFrame(CallStackNode node, TraceCallStack callStack)
+        {
+            var caller = callStack.Caller;
+            if (caller == null)
+            {
+                // root node
+                node.CallsCount = node.CallsCount + 1;
+            }
+            var childNode = caller == null ? AddOrUpdateChildNode(node, callStack) : ProcessStackFrame(node, caller);
+            return AddOrUpdateChildNode(childNode, callStack);
+        }
+
+        private static CallStackNode BuildCallStackTree(TraceLog traceLog, int pid, int tid)
+        {
+            var perfInfoTaskGuid = new Guid(0xce1dbfb4, 0x137e, 0x4da6, 0x87, 0xb0, 0x3f, 0x59, 0xaa, 0x10, 0x2c, 0xbc);
+            var perfInfoOpcode = 46;
+
+            var callStacks = new CallStackNode(0);
+            foreach (var ev in traceLog.Events.Where(ev => ev.ProcessID == pid && ev.ThreadID == tid
+                            && ev.TaskGuid == perfInfoTaskGuid && (int)ev.Opcode == perfInfoOpcode))
+            {
+                ProcessStackFrame(callStacks, ev.CallStack());
+            }
+
+            return callStacks;
+        }
+
+        private static void PrintCallStacks(string threadDesc, int maxDepth, CallStackNode node)
+        {
+            void PrintCallStacksRecursive(string name, int depth, CallStackNode node)
+            {
+                var indentation = String.Concat(Enumerable.Repeat("| ", depth));
+                Console.WriteLine($"{indentation}├─ {name} [{node.CallsCount}]");
+                if (node.Count == 0 || (maxDepth > 0 && depth >= maxDepth)) {
+                    return;
+                }
+                foreach (var kv in node) {
+                    PrintCallStacksRecursive(kv.Key, depth + 1, kv.Value);
+                }
+            }
+            PrintCallStacksRecursive(threadDesc, 0, node);
+        }
+
+        private static void Analyze(string processNameOrPid, int maxDepth)
+        {
+            var reader = new ZippedETLReader(etlFilePath + ".zip", log);
+            reader.UnpackArchive();
+
+            var options = new TraceLogOptions() { ConversionLog = log };
+            var traceLog = TraceLog.OpenOrConvert(etlFilePath, options);
+
+            if (!TryFindProcess(traceLog, processNameOrPid, out var proc))
+            {
+                Console.WriteLine("No matching process found in the trace");
+                return;
+            }
+
+            Console.WriteLine($"Analyzing {proc.Name} ({proc.ProcessID}): '{proc.CommandLine}'");
+
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine($"[{sw.ElapsedMilliseconds} ms] Loading symbols for process modules");
+            LoadSymbols(traceLog, proc);
+
+            // usually, system process has PID 4, but TraceEvent attaches the drivers to the Idle process (0)
+            var systemProc = traceLog.Processes.First(p => p.ProcessID == 0);
+            Console.WriteLine($"[{sw.ElapsedMilliseconds} ms] Loading symbols for system modules");
+            LoadSymbols(traceLog, systemProc);
+
+            Console.WriteLine($"[{sw.ElapsedMilliseconds} ms] Starting call stack analysis");
+
+            foreach (var thr in proc.Threads)
+            {
+                var pid = thr.Process.ProcessID;
+                var callStacks = BuildCallStackTree(traceLog, pid, thr.ThreadID);
+
+                PrintCallStacks($"Thread ({thr.ThreadID}) '{thr.ThreadInfo}'", maxDepth, callStacks);
+            }
+            Console.WriteLine($"[{sw.ElapsedMilliseconds} ms] Completed call stack analysis");
+        }
+    }
+}
---
+++ cpu-sampling-profiler/fsharp/Program.fs
@@ -0,0 +1,187 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open System.Threading
+open Microsoft.Diagnostics.Tracing
+open Microsoft.Diagnostics.Tracing.Parsers
+open Microsoft.Diagnostics.Tracing.Parsers.Clr
+open Microsoft.Diagnostics.Tracing.Session
+open Microsoft.Diagnostics.Tracing.Etlx
+open Microsoft.Diagnostics.Symbols
+open Microsoft.FSharp.Linq
+open System.Diagnostics
+
+type NtKeywords = KernelTraceEventParser.Keywords
+type ClrKeywords = ClrTraceEventParser.Keywords
+
+let sessionName = "mytrace"
+let etlFilePath = sprintf "%s.etl" sessionName
+
+let log = 
+    if isNull (Environment.GetEnvironmentVariable("ENABLE_TRACEEVENT_LOG")) then TextWriter.Null
+    else Console.Out
+
+let startTrace () =
+    let collectClrRundownEvents () = 
+        let rec waitForRundownEvents filePath =
+            // Poll until 2 second goes by without growth.
+            let len = FileInfo(filePath).Length
+            Thread.Sleep(2000)
+            if FileInfo(filePath).Length <> len then
+                waitForRundownEvents filePath
+            else ()
+
+        printfn "Forcing rundown of JIT methods."
+        let rundownFileName = Path.ChangeExtension(etlFilePath, ".clrRundown.etl")
+        use rundownSession = new TraceEventSession(sessionName + "-rundown", rundownFileName)
+        rundownSession.EnableProvider(ClrRundownTraceEventParser.ProviderGuid, 
+            TraceEventLevel.Verbose, uint64(ClrRundownTraceEventParser.Keywords.Default)) |> ignore
+        waitForRundownEvents rundownFileName
+        printfn "Done with rundown."
+
+    do
+        use session = new TraceEventSession(sessionName, etlFilePath)
+
+        let traceFlags = NtKeywords.Profile ||| NtKeywords.ImageLoad // NtKeywords.Process and NtKeywords.Thread are added automatically
+        let stackFlags = NtKeywords.Profile
+        session.EnableKernelProvider(traceFlags, stackFlags) |> ignore
+        
+        // we need CLR to resolve managed stacks
+        let keywords = ClrKeywords.JITSymbols
+        session.EnableProvider(ClrTraceEventParser.ProviderGuid, TraceEventLevel.Always, uint64(keywords)) |> ignore
+       
+        printfn "Press enter to stop the tracing session"
+        Console.ReadLine() |> ignore
+
+        session.Stop() |> ignore
+
+        collectClrRundownEvents ()
+
+    printfn "Collecting the data required for stack resolution..."
+    let writer = ZippedETLWriter(etlFilePath, log)
+    if not (writer.WriteArchive(Compression.CompressionLevel.NoCompression)) then
+        printfn "Error occured while merging the data"
+    else
+        printfn "Trace session completed"
+
+type CallStackNode (callsCount : int32) =
+    inherit Dictionary<string, CallStackNode>(StringComparer.OrdinalIgnoreCase)
+    member val CallsCount = callsCount with get, set
+
+let loadSymbols (traceLog : TraceLog) (proc : TraceProcess) =
+    use symbolReader = new SymbolReader(log)
+
+    proc.LoadedModules |> Seq.where (fun m -> not (isNull m.ModuleFile))
+                       |> Seq.iter (fun m -> traceLog.CodeAddresses.LookupSymbolsForModule(symbolReader, m.ModuleFile))
+
+let buildCallStacksTree (traceLog : TraceLog) pid tid =
+    let perfInfoTaskGuid = Guid(int32 0xce1dbfb4, int16 0x137e, int16 0x4da6, byte 0x87, byte 0xb0, byte 0x3f, byte 0x59, byte 0xaa, byte 0x10, byte 0x2c, byte 0xbc)
+    let perfInfoOpcode = 46
+
+    let callStacks = CallStackNode(0)
+    let processCallStack (callStack : TraceCallStack) =
+        let addOrUpdateChildNode (node : CallStackNode) (callStack : TraceCallStack) =
+            let decodedAddress = sprintf "%s!%s" callStack.CodeAddress.ModuleName callStack.CodeAddress.FullMethodName
+            match node.TryGetValue(decodedAddress) with
+            | (true, childNode) ->
+                childNode.CallsCount <- childNode.CallsCount + 1
+                childNode
+            | (false, _) ->
+                let childNode = CallStackNode(1)
+                node.Add(decodedAddress, childNode)
+                childNode
+
+        let rec processStackFrame (callStackNode : CallStackNode) (callStack : TraceCallStack) =
+            let caller = callStack.Caller
+            if isNull caller then // root node
+                callStackNode.CallsCount <- callStackNode.CallsCount + 1
+            
+            let childNode = 
+                if isNull caller then addOrUpdateChildNode callStackNode callStack
+                else processStackFrame callStackNode caller
+
+            addOrUpdateChildNode childNode callStack
+        processStackFrame callStacks callStack |> ignore
+    
+    traceLog.Events
+    |> Seq.filter (fun ev -> ev.ProcessID = pid && ev.ThreadID = tid && ev.TaskGuid = perfInfoTaskGuid && (int32 ev.Opcode = perfInfoOpcode))
+    |> Seq.iter (fun ev -> processCallStack (ev.CallStack()))
+
+    callStacks
+
+let tryFindProcess (traceLog : TraceLog) processNameOrPid =
+    let (|Pid|ProcessName|) (s : string) =
+        match Int32.TryParse(s) with
+        | (true, pid) -> Pid pid
+        | (false, _) -> ProcessName s
+
+    let filter =
+        match processNameOrPid with
+        | Pid pid -> fun (p : TraceProcess) -> p.ProcessID = pid
+        | ProcessName name -> fun (p : TraceProcess) -> String.Equals(p.Name, name, StringComparison.OrdinalIgnoreCase)
+
+    let processes = traceLog.Processes |> Seq.where filter |> Seq.toArray
+    if processes.Length = 0 then
+        None
+    elif processes.Length = 1 then
+        Some processes.[0]
+    else
+        processes |> Seq.iteri (fun i p -> printfn "[%d] %s (%d): '%s'" i p.Name p.ProcessID p.CommandLine)
+        printf "Which process to analyze []: "
+        match Int32.TryParse(Console.ReadLine()) with
+        | (false, _) -> None
+        | (true, n) -> processes |> Seq.tryItem n
+
+let analyze processNameOrPid maxDepth =
+
+    let reader = ZippedETLReader(etlFilePath + ".zip", log)
+    reader.UnpackArchive()
+    
+    let options = TraceLogOptions(ConversionLog = log)
+    let traceLog = TraceLog.OpenOrConvert(etlFilePath, options)
+
+    match tryFindProcess traceLog processNameOrPid with
+    | None -> printfn "No matching process found in the trace"
+    | Some proc ->
+        printfn "%s [%s] (%d)" proc.Name proc.CommandLine proc.ProcessID
+
+        let sw = Stopwatch.StartNew()
+        printfn "[%6d ms] Loading symbols for modules in process %s (%d)" sw.ElapsedMilliseconds proc.Name proc.ProcessID
+        loadSymbols traceLog proc
+
+        // usually, system process has PID 4, but TraceEvent attaches the drivers to the Idle process (0)
+        let systemProc = traceLog.Processes |> Seq.where (fun p -> p.ProcessID = 0) |> Seq.exactlyOne
+
+        printfn "[%6d ms] Loading symbols for system modules" sw.ElapsedMilliseconds
+        loadSymbols traceLog systemProc
+
+        printfn "[%6d ms] Starting call stack analysis" sw.ElapsedMilliseconds
+        let printThreadCallStacks (thread : TraceThread) =
+            let callStacks = buildCallStacksTree traceLog proc.ProcessID thread.ThreadID
+
+            let rec getCallStack depth name (callStack : CallStackNode) =
+                let folder frames (kv : KeyValuePair<string, CallStackNode>) =
+                    if kv.Value.Count = 0 || (maxDepth > 0 && depth >= maxDepth) then frames
+                    else getCallStack (depth + 1) kv.Key kv.Value |> List.append frames
+
+                callStack
+                |> Seq.fold folder [ (sprintf "%s├─ %s [%d]" ("│ " |> String.replicate depth) name callStack.CallsCount) ]
+
+            let stacks = getCallStack 0 (sprintf "Thread (%d) '%s'" thread.ThreadID thread.ThreadInfo) callStacks
+            stacks |> List.iter (fun s -> printfn "%s" s)
+        
+        proc.Threads |> Seq.iter printThreadCallStacks
+        printfn "[%6d ms] Completed call stack analysis" sw.ElapsedMilliseconds
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length = 1 && argv.[0] = "trace" then
+        if TraceEventSession.IsElevated() ?= true then
+            startTrace ()
+        else
+            printfn "ERROR: You must be admin to trace the system."
+    elif argv.Length >= 2 && argv.[0] = "analyze" then
+        analyze argv.[1] (if argv.Length > 2 then Int32.Parse(argv.[2]) else 0)
+    else
+        printfn "Usage: etwprofiler trace | analyze <pid|process-name> [max-depth]"
+    0
